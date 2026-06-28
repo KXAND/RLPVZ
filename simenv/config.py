2a6277ecb4f5847980ad30c8894a69bfdbec4467
KXAND <71508678+KXAND@users.noreply.github.com>
2026-06-28 23:24:36 +0800

    feat(sim): add configurable difficulty and reward settings

---
+++ simenv/config.py
@@ -0,0 +1,72 @@
+DIFFICULTY = {
+    "fps": 2,
+    "max_frames": 400,
+    "rows": 5,
+    "cols": 9,
+    "initial_sun": 50,
+    "natural_sun_production": 25,
+    "natural_sun_cooldown": 10,
+    "mowers": False,
+    "survival_score": 0,
+    "survival_step": 20,
+    "alive_plant_score": 0,
+    "alive_mower_score": 0,
+}
+
+
+ZOMBIE_SPAWN = {
+    "initial_offset_sec": 10,
+    "spawn_interval_sec": 8,
+    "first_wave_delay_multiplier": 10,
+    "wave_interval_multiplier": 20,
+    "post_wave_spawn_delay_multiplier": 10,
+    "base_advanced_probability": 0.05,
+    "advanced_probability_growth": 2.0,
+    "max_advanced_probability": 1.0,
+    "cone_probability_multiplier": 3.0,
+}
+
+
+REWARDS = {
+    "zombie_kill": {
+        "use_type_rewards": False,
+        "default": 0.30,
+        "zombie": 0.30,
+        "flag": 0.30,
+        "conehead": 0.45,
+        "buckethead": 0.70,
+    },
+    "sun_collect": 0.01,
+    "wave_complete": 4.0,
+    "game_win": 18.0,
+    "streak_bonus": 0.0,
+    "survival_per_step": 0.0,
+    "plant_sunflower": 0.10,
+    "plant_attacker": 0.35,
+    "plant_wall": 0.18,
+    "plant_other": 0.30,
+    "plant_lost": -0.25,
+    "sunflower_lost": -0.80,
+    "lawnmower_triggered": -50.0,
+    "game_lose": -12.0,
+    "invalid_action": -0.01,
+    "wait_with_sun": -0.02,
+    "wait_sun_threshold": 300,
+    "coverage": {
+        "scale": 0.0,
+    },
+    "proximity": {
+        "scale": 0.0,
+    },
+    "potential": {
+        "sun_scale": 0.06,
+        "sun_cap": 300.0,
+        "plant_scale": 0.22,
+        "spread_bonus": 0.06,
+        "lawnmower_scale": 0.35,
+        "zombie_threat_scale": 0.35,
+        "zombie_distance_bonus": 0.75,
+        "wave_scale": 0.05,
+        "delta_scale": 0.18,
+    },
+}
--- simenv/pvz_sim/entities/zombies/wave_zombie_spawner.py
+++ simenv/pvz_sim/entities/zombies/wave_zombie_spawner.py
@@ -4,17 +4,23 @@ from .zombie_cone import Zombie_cone
 from .zombie_bucket import Zombie_bucket
 from .zombie_flag import Zombie_flag
 from ... import config
+from simenv.config import ZOMBIE_SPAWN
 import random
 
-INITIAL_OFFSET = 10
-SPAWN_INTERVAL = 8
+INITIAL_OFFSET = ZOMBIE_SPAWN["initial_offset_sec"]
+SPAWN_INTERVAL = ZOMBIE_SPAWN["spawn_interval_sec"]
 
 
 class WaveZombieSpawner(ZombieSpawner):
     def __init__(self):
         self._timer = INITIAL_OFFSET * config.FPS - 1
-        self._wave_timer = 10 * INITIAL_OFFSET * config.FPS - 1
-        self.p = 0.05
+        self._wave_timer = (
+            ZOMBIE_SPAWN["first_wave_delay_multiplier"]
+            * INITIAL_OFFSET
+            * config.FPS
+            - 1
+        )
+        self.p = ZOMBIE_SPAWN["base_advanced_probability"]
 
     def spawn(self, scene):
         if self._timer <= 0 and self._wave_timer > 0:
@@ -22,7 +28,7 @@ class WaveZombieSpawner(ZombieSpawner):
             s = random.random()
             if s < self.p:
                 scene.add_zombie(Zombie_bucket(lane))
-            elif s < 3 * self.p:
+            elif s < ZOMBIE_SPAWN["cone_probability_multiplier"] * self.p:
                 scene.add_zombie(Zombie_cone(lane))
             else:
                 scene.add_zombie(Zombie(lane))
@@ -37,10 +43,23 @@ class WaveZombieSpawner(ZombieSpawner):
                     s = random.random()
                     if s < self.p:
                         scene.add_zombie(Zombie_bucket(lane))
-                    elif s < 3 * self.p:
+                    elif s < ZOMBIE_SPAWN["cone_probability_multiplier"] * self.p:
                         scene.add_zombie(Zombie_cone(lane))
                     else:
                         scene.add_zombie(Zombie(lane))
-                self._wave_timer = 20 * SPAWN_INTERVAL * config.FPS - 1
-                self._timer = 10 * INITIAL_OFFSET * config.FPS - 1
-                self.p = min(self.p * 2, 1)
+                self._wave_timer = (
+                    ZOMBIE_SPAWN["wave_interval_multiplier"]
+                    * SPAWN_INTERVAL
+                    * config.FPS
+                    - 1
+                )
+                self._timer = (
+                    ZOMBIE_SPAWN["post_wave_spawn_delay_multiplier"]
+                    * INITIAL_OFFSET
+                    * config.FPS
+                    - 1
+                )
+                self.p = min(
+                    self.p * ZOMBIE_SPAWN["advanced_probability_growth"],
+                    ZOMBIE_SPAWN["max_advanced_probability"],
+                )
